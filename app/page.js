5627f920c8fe4e5e9bfd0c623cedd4505bc4d4d0
hannnnxiiii <hanxi_lin@outlook.com>
2025-07-22 19:03:27 +0800

    feat: 再再次尝试自动化部署

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <main className="max-w-2xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">林晗曦的网络日志</h1>
+      <h1 className="text-2xl font-bold mb-4">林晗曦的网络日志^^</h1>
       <ul>
         {posts.map((post) => (
           <li key={post.id} className="mb-2">
