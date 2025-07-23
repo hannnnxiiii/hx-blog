9646280f934f9b5da7238a7a57671d9368e54938
hannnnxiiii <hanxi_lin@outlook.com>
2025-07-23 19:39:17 +0800

    feat: 测试自动化部署

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 import Link from 'next/link'
 
 export default async function Home() {
-  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' })
+  const res = await fetch('http://localhost:3000/api/posts', {
+    cache: 'no-store',
+  })
   const posts = await res.json()
 
   return (
     <main className="max-w-2xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">林晗曦的网络日志^^</h1>
+      <h1 className="text-2xl font-bold mb-4">林晗曦的网络日志 ^^</h1>
       <ul>
         {posts.map((post) => (
           <li key={post.id} className="mb-2">
