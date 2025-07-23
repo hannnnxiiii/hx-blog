78d0675cd8b9c6cc3e83b2eaa1bff7910320f5f2
hannnnxiiii <hanxi_lin@outlook.com>
2025-07-23 18:35:26 +0800

    fix: 同步数据库修改

--- prisma/migrations/20250723100046_update_post_content/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `post` MODIFY `content` TEXT NOT NULL;
--- prisma/migrations/20250723103315_init/migration.sql
+++ prisma/migrations/20250723103315_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `Post` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
-    `content` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
