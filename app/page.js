ef984449068c577f41df58b0f748d3a90a286f88
hannnnxiiii <hanxi_lin@outlook.com>
2025-07-24 16:11:55 +0800

    feat: 对游客隐藏编辑按钮

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 // app/page.tsx
 import Link from 'next/link'
 import dayjs from 'dayjs'
+import getServerAuth from '@/lib/auth'
 
 export default async function Home() {
   const res = await fetch('http://localhost:3000/api/posts', {
     cache: 'no-store',
   })
   const posts = await res.json()
+  const isAuthenticated = await getServerAuth()
 
   return (
     <main className="max-w-5xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-4">林晗曦的网络日志 ^^</h1>
-      <ul>
+      <ul className="border-t-2 border-gray-200">
         {posts.map((post) => (
           <li
             key={post.id}
@@ -19,32 +21,33 @@ export default async function Home() {
           >
             <Link
               href={`/blog/${post.id}`}
-              className="p-2 hover:text-blue-500 hover:underline transition-colors flex-1 flex justify-between items-center"
+              className="p-2 hover:text-blue-500 transition-colors flex-1 flex justify-between items-center"
             >
               <span>{post.title}</span>
               <span>{dayjs(post.createdAt).format('YYYY-MM-DD')}</span>
             </Link>
-            <span className="flex items-center gap-2 px-2 border-l-2 border-gray-200">
-              <Link href={`/admin?id=${post.id}`}>
-                <button className="hover:text-blue-500 hover:underline flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                    />
-                  </svg>
-                  编辑
-                </button>
+            {isAuthenticated && (
+              <Link
+                href={`/admin?id=${post.id}`}
+                className="flex items-center gap-1 p-2 border-l-2 border-gray-200 hover:text-blue-500 transition-colors"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                  />
+                </svg>
+                编辑
               </Link>
-            </span>
+            )}
           </li>
         ))}
       </ul>
