da8c64c204c53e3e6ec885db22bc826aa2ea5b3c
hannnnxiiii <hanxi_lin@outlook.com>
2025-07-25 17:37:47 +0800

    feat: 改返回首页按钮为悬浮

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -20,35 +20,39 @@ export default async function PostDetail({ params }) {
   const contentHtml = processedContent.toString()
 
   return (
-    <main className="max-w-5xl mx-auto p-6">
-      <Link
-        href="/"
-        className="text-blue-600 flex gap-1 items-center hover:underline"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-4"
+    <>
+      <nav className="sticky top-0 bg-white py-4 shadow-[0_4px_4px_0_rgba(0,0,0,.08)]">
+        <Link
+          href="/"
+          className="text-blue-600 flex gap-1 items-center hover:underline max-w-5xl mx-auto px-6"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-          />
-        </svg>
-        返回首页
-      </Link>
-      <h1 className="text-4xl font-bold my-8">{post.title}</h1>
-      <p className="text-sm text-gray-500 mb-4">
-        {new Date(post.createdAt).toLocaleString()}
-      </p>
-      <div
-        className="prose max-w-none"
-        dangerouslySetInnerHTML={{ __html: contentHtml }}
-      />
-    </main>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+          返回首页
+        </Link>
+      </nav>
+      <main className="max-w-5xl mx-auto p-6">
+        <h1 className="text-4xl font-bold my-8">{post.title}</h1>
+        <p className="text-sm text-gray-500 mb-4">
+          {new Date(post.createdAt).toLocaleString()}
+        </p>
+        <div
+          className="prose max-w-none"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </main>
+    </>
   )
 }
